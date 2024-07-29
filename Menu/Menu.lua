ac2ce6716fbad48f44a84a6765dead624e16b993
Vollmer <kristianvollmer@gmail.com>
2024-07-29 16:49:57 +0200

    re-impl layout swap hook

--- Menu/Menu.lua
+++ Menu/Menu.lua
@@ -4,6 +4,7 @@ local CUF = select(2, ...)
 local Cell = CUF.Cell
 local L = Cell.L
 local P = Cell.pixelPerfectFuncs
+local Util = CUF.Util
 
 local menuWindow = CUF.MenuWindow
 
@@ -16,6 +17,8 @@ menu.widgetsToAdd = {}
 menu.selectedLayout = Cell.vars.currentLayout
 menu.selectedLayoutTable = Cell.vars.currentLayoutTable
 menu.selectedUnit = "player"
+menu.init = false
+menu.hookInit = false
 
 CUF.Menu = menu
 
@@ -43,8 +46,36 @@ local function ShowTab(tab)
         LoadLayoutDB(Cell.vars.currentLayout)
 
         menuWindow:ShowMenu()
+
+        if not menu.init then
+            menu.init = true
+        end
     else
         menuWindow:HideMenu()
     end
 end
 Cell:RegisterCallback("ShowOptionsTab", "CellUnitFrames_ShowTab", ShowTab)
+
+-- This is hacky, but it works
+-- This is needed to get access to current layout, since this
+-- is the only place where this info is outside of local scope
+local function UpdatePreview()
+    if not menu.init or menu.hookInit then return end
+
+    local layoutsTab = Util.findChildByName(Cell.frames.optionsFrame, "CellOptionsFrame_LayoutsTab")
+    local layoutPane = Util.findChildByName(layoutsTab, "Layout")
+    local layoutDropdown = Util.findChildByProp(layoutPane, "items")
+
+    hooksecurefunc(layoutDropdown, "SetSelected", function(self)
+        LoadLayoutDB(self:GetSelected())
+    end)
+    hooksecurefunc(layoutDropdown, "SetSelectedValue", function(self)
+        LoadLayoutDB(self:GetSelected())
+    end)
+
+    LoadLayoutDB(layoutDropdown:GetSelected())
+    Cell:UnregisterCallback("UpdatePreview", "CellUnitFrames_UpdatePreview")
+
+    menu.hookInit = true
+end
+Cell:RegisterCallback("UpdatePreview", "CellUnitFrames_UpdatePreview", UpdatePreview)

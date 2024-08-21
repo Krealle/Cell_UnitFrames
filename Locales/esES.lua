cf06021ce510f9d7d50f69052e1e79c080c70194
Vollmer <kristianvollmer@gmail.com>
2024-08-21 17:50:36 +0200

    add missing locales

---
+++ Locales/esES.lua
@@ -0,0 +1,4 @@
+if (GAME_LOCALE or GetLocale()) ~= "esES" then return end
+local L = CUF.L
+
+--@localization(locale="esES", format="lua_additive_table")@
---
+++ Locales/esMX.lua
@@ -0,0 +1,4 @@
+if (GAME_LOCALE or GetLocale()) ~= "esMX" then return end
+local L = CUF.L
+
+--@localization(locale="esMX", format="lua_additive_table")@

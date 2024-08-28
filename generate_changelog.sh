3b397964b6750dff1cafaaf51e588b721cb8c670
Vollmer <kristianvollmer@gmail.com>
2024-08-28 18:44:01 +0200

    update generate_changelog

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -26,8 +26,9 @@ url=$( git remote get-url origin | sed -e 's/^git@\(.*\):/https:\/\/\1\//' -e 's
 echo -ne "# [${version}](${url}/tree/${current}) ($date)\n\n[Full Changelog](${url}/compare/${previous}...${current})\n\n" > "CHANGELOG.md"
 
 if [ "$version" = "$tag" ]; then # on a tag
-  highlights=$( git cat-file -p "$tag" | sed -e '1,5d' -e '/^-----BEGIN PGP/,/^-----END PGP/d' )
-  echo -ne "## Highlights\n\n ${highlights} \n\n## Commits\n\n" >> "CHANGELOG.md"
+  # Fetch release notes using GitHub CLI
+  releaseNotes=$(gh release view "$tag" --json body -q ".body")
+  echo -ne "## Release Notes\n\n ${releaseNotes} \n\n## Commits\n\n" >> "CHANGELOG.md"
 fi
 
 git shortlog --no-merges --reverse "$previous..$current" | sed -e  '/^\w/G' -e 's/^      /- /' >> "CHANGELOG.md"

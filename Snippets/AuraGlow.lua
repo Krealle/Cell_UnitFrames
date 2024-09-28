b9eca1fc52a0692003089aa057ff8c61d6a8c236
Vollmer <kristianvollmer@gmail.com>
2024-09-28 15:46:25 +0200

    Update AuraGlow.lua

--- Snippets/AuraGlow.lua
+++ Snippets/AuraGlow.lua
@@ -1,3 +1,11 @@
+-- Apply Glow to buffs with specific spellIDs
+--
+-- See documentation for Glow functions here:
+-- https://github.com/Krealle/Cell_UnitFrames/blob/2ac6b435c5be1ee1f1749773bce6b279df1bc1d4/Util/Utils.lua#L498
+--
+-- `auraData` arg being passed is in this format:
+-- https://warcraft.wiki.gg/wiki/API_C_UnitAuras.GetAuraDataByIndex
+
 -- Run Snippet late to ensure addon is properly loaded
 EventUtil.RegisterOnceFrameEventAndCallback("LOADING_SCREEN_DISABLED", function()
     local Util = CUF.Util

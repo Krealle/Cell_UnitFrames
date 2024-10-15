ede5c8509c77d1186e71d9dbf764cb36d5bd5042
Vollmer <kristianvollmer@gmail.com>
2024-10-15 21:04:36 +0200

    create example snippets for increasing max aura icons

---
+++ Snippets/IncreaseMaxAuraIcons.lua
@@ -0,0 +1,9 @@
+-- Increase the maximum number of aura icons that can be shown
+local function IncreaseMaxAuraIcons()
+    -- Make sure CUF is actually loaded
+    local CUF = _G["CUF"]
+    if not CUF then return end
+
+    CUF.Defaults.Values.maxAuraIcons = 40
+end
+Cell:RegisterCallback("CUF_AddonLoaded", "Snippet_IncreaseMaxAuraIcons", IncreaseMaxAuraIcons)

394b6a2f0b07dec1a76e8c8ec6769a5df9014e5c
Vollmer <kristianvollmer@gmail.com>
2024-10-23 15:33:33 +0200

    rework Unit Frames tab to have sub tabs

--- Menu/UnitFramesTab.lua
+++ Menu/UnitFramesTab.lua
@@ -14,24 +14,17 @@ local Menu = CUF.Menu
 ---@class UnitsFramesTab: Menu.Tab
 ---@field unitPages table<Unit, UnitsMenuPage>
 ---@field unitPageButtons UnitMenuPageButton[]
----@field widgetPages table<WIDGET_KIND, WidgetMenuPage>
----@field listButtons table<WIDGET_KIND, CellButton>
 ---@field selectedWidgetTable WidgetTable
 ---@field unitsToAdd table<number, function>
----@field widgetsToAdd table<number, WidgetMenuPage.Args>
 local unitFramesTab = {}
 unitFramesTab.id = "unitFramesTab"
 unitFramesTab.unitPages = {}
 unitFramesTab.unitsToAdd = {}
-unitFramesTab.widgetPages = {}
-unitFramesTab.listButtons = {}
-unitFramesTab.widgetsToAdd = {}
 unitFramesTab.unitPageButtons = {}
-unitFramesTab.firstWidgetInList = nil
-unitFramesTab.widgetHeight = 400
 unitFramesTab.unitHeight = 180
 unitFramesTab.paneHeight = 17
 
+Menu.unitFramesTab = unitFramesTab
 Menu:AddTab(unitFramesTab)
 
 -------------------------------------------------
@@ -52,121 +45,11 @@ function unitFramesTab:SetUnitPage(unit)
     self.selectedUnitPage = self.unitPages[unit]
     self.selectedUnitPage.frame:Show()
 
-    self.LoadWidgetList(unit)
+    if self.selectedTab then
+        self.selectedTab:ShowTab(unit)
+    end
     CUF.Menu:UpdateSelectedPages(unit)
 end
-
--- Update the selected widge
----@param widget WIDGET_KIND
-function unitFramesTab:SetWidget(widget)
-    -- Hide old widget
-    if self.selectedWidget then
-        self.selectedWidget.frame:Hide()
-    end
-
-    self.selectedWidget = self.widgetPages[widget]
-    self.selectedWidget.frame:Show()
-
-    self.selectedWidget.frame:ClearAllPoints()
-    self.selectedWidget.frame:SetPoint("TOPLEFT", self.settingsFrame.scrollFrame.content)
-
-    -- Extremely dirty hack, but without this options will sometimes not be shown
-    C_Timer.After(0.1, function()
-        self.settingsFrame.scrollFrame:SetContentHeight(self.selectedWidget.height)
-        self.settingsFrame.scrollFrame:ResetScroll()
-    end)
-
-    CUF.Menu:UpdateSelectedPages(nil, widget)
-end
-
----@param unit Unit
-function unitFramesTab.LoadWidgetList(unit)
-    if not unitFramesTab:IsShown() then return end
-
-    unitFramesTab.widgetListFrame.scrollFrame:Reset()
-
-    local optionCount = 0
-    local widgetTable = CUF.DB.GetSelectedWidgetTables(unit)
-    local prevButton
-    unitFramesTab.firstWidgetInList = nil
-
-    -- The list is ordered by load order from .toc
-    for _, widgetPage in pairs(unitFramesTab.widgetsToAdd) do
-        local widgetName = widgetPage.widgetName
-        local widget = widgetTable[widgetName]
-
-        if widget then
-            if not unitFramesTab.listButtons[widgetName] then
-                unitFramesTab.listButtons[widgetName] = CUF:CreateButton(
-                    unitFramesTab.widgetListFrame.scrollFrame.content, " ",
-                    { 20, 20 }, nil, "transparent-accent")
-            end
-
-            ---@class WidgetMenuPageButton: CellButton
-            local button = unitFramesTab.listButtons[widgetName]
-            button:SetText(L[widgetName])
-            button:GetFontString():ClearAllPoints()
-            button:GetFontString():SetPoint("LEFT", 5, 0)
-            button:GetFontString():SetPoint("RIGHT", -5, 0)
-
-            button.id = widgetName
-            optionCount = optionCount + 1
-
-            if widget.enabled then
-                button:SetTextColor(1, 1, 1, 1)
-            else
-                button:SetTextColor(0.466, 0.466, 0.466, 1)
-            end
-
-            button:SetParent(unitFramesTab.widgetListFrame.scrollFrame.content)
-            button:SetPoint("RIGHT")
-            if not prevButton then
-                button:SetPoint("TOPLEFT")
-            else
-                button:SetPoint("TOPLEFT", prevButton, "BOTTOMLEFT", 0, 1)
-            end
-            button:Show()
-
-            prevButton = button
-            if not unitFramesTab.firstWidgetInList then
-                unitFramesTab.firstWidgetInList = widgetName
-            end
-        end
-    end
-
-    unitFramesTab.widgetListFrame.scrollFrame:SetContentHeight(20, optionCount, -1)
-
-    Cell:CreateButtonGroup(unitFramesTab.listButtons, function(widget, b)
-        unitFramesTab:SetWidget(widget)
-    end)
-
-    -- Make sure that the currently selected widget is valid
-    if unitFramesTab.selectedWidget then
-        if not widgetTable[unitFramesTab.selectedWidget.id] then
-            unitFramesTab.listButtons[unitFramesTab.firstWidgetInList]:Click()
-        end
-    end
-end
-
-CUF:RegisterCallback("LoadPageDB", "unitFramesTab_LoadWidgetList", unitFramesTab.LoadWidgetList)
-
----@param layout string?
----@param unit Unit?
----@param widgetName WIDGET_KIND?
-function unitFramesTab.UpdateWidgetListEnabled(layout, unit, widgetName, setting)
-    if not unitFramesTab:IsShown() then return end
-    if not widgetName then return end
-    if not setting == CUF.constants.OPTION_KIND.ENABLED then return end
-    if not unitFramesTab.listButtons[widgetName] then return end
-
-    if CUF.DB.GetSelectedWidgetTable(widgetName, unit).enabled then
-        unitFramesTab.listButtons[widgetName]:SetTextColor(1, 1, 1, 1)
-    else
-        unitFramesTab.listButtons[widgetName]:SetTextColor(0.466, 0.466, 0.466, 1)
-    end
-end
-
-CUF:RegisterCallback("UpdateWidget", "unitFramesTab_UpdateWidgetListEnabled", unitFramesTab.UpdateWidgetListEnabled)
 
 function unitFramesTab:ShowTab()
     CUF:Log("|cff00ccffShow unitFramesTab|r")
@@ -176,7 +59,9 @@ function unitFramesTab:ShowTab()
         self.window:Show()
 
         self.unitPageButtons[1]:Click()
-        self.listButtons[self.firstWidgetInList]:Click()
+        self.tabButtons[1]:Click()
+
+        CUF.Menu:UpdateSelectedPages(CUF.vars.selectedUnit, CUF.vars.selectedWidget)
 
         self.init = true
 
@@ -241,25 +126,76 @@ function unitFramesTab:InitUnits()
     end
 end
 
-function unitFramesTab:InitWidgets()
-    for _, widget in pairs(self.widgetsToAdd) do
-        local widgetPage = Builder:CreateWidgetMenuPage(self.settingsFrame, widget.widgetName, unpack(widget.options))
-
-        self.widgetPages[widgetPage.id] = widgetPage
-    end
-end
-
 ---@param unit function
 function Menu:AddUnit(unit)
     table.insert(unitFramesTab.unitsToAdd, unit)
 end
 
----@param widgetName WIDGET_KIND
----@param ... MenuOptions
-function Menu:AddWidget(widgetName, ...)
-    table.insert(unitFramesTab.widgetsToAdd,
-        { ["widgetName"] = widgetName, ["options"] = { ... } })
+-------------------------------------------------
+-- MARK: Tabs
+-------------------------------------------------
+
+---@type Menu.Tab[]
+unitFramesTab.tabsToAdd = {}
+---@type table<string, Menu.Tab>
+unitFramesTab.tabs = {}
+---@type Menu.TabButton[]
+unitFramesTab.tabButtons = {}
+
+--- Initialize tabs buttons and adds them to the tab pane
+function unitFramesTab:InitTabs()
+    --CUF:Log("menu - InitUnits")
+    local prevButton
+    local prevAnchor
+    local idx = 1
+
+    for _, tab in pairs(self.tabsToAdd) do
+        ---@cast tab Menu.Tab
+        self.tabs[tab.id] = tab
+
+        ---@class Menu.TabButton: CellButton
+        local tabButton = CUF:CreateButton(self.window, L[tab.id], { 85, 17 })
+        tabButton.id = tab.id
+
+        if prevButton then
+            -- Max 4 buttons per row
+            if idx % 4 == 0 then
+                tabButton:SetPoint("BOTTOMRIGHT", prevAnchor, "TOPRIGHT", 0, 0)
+                idx = 1
+                prevAnchor = tabButton
+
+                self.window:SetHeight(self.window:GetHeight() + self.paneHeight)
+                self.tabPane:SetHeight(self.tabPane:GetHeight() + self.paneHeight)
+            else
+                tabButton:SetPoint("TOPLEFT", prevButton, "TOPRIGHT", -1, 0)
+                idx = idx + 1
+            end
+        else
+            tabButton:SetPoint("BOTTOMLEFT", self.tabPane, "BOTTOMLEFT", 0, 0)
+            prevAnchor = tabButton
+        end
+        prevButton = tabButton
+
+        table.insert(self.tabButtons, tabButton)
+    end
+
+    Cell:CreateButtonGroup(self.tabButtons, function(which, b)
+        self:SetTab(which)
+    end)
 end
+
+---@param which string
+function unitFramesTab:SetTab(which)
+    -- Hide old tab
+    if self.selectedTab then
+        self.selectedTab:HideTab()
+    end
+
+    self.selectedTab = self.tabs[which]
+    self.selectedTab:ShowTab(CUF.vars.selectedUnit)
+end
+
+unitFramesTab.AddTab = Menu.AddTab
 
 -------------------------------------------------
 -- MARK: Create
@@ -268,8 +204,7 @@ end
 function unitFramesTab:Create()
     CUF:Log("|cff00ccffCreate UnitFramesTab|r")
 
-    local inset = Menu.inset
-    local windowHeight = self.unitHeight + self.widgetHeight + (self.paneHeight * 2)
+    local windowHeight = self.unitHeight + 400 + (self.paneHeight * 2)
 
     local sectionWidth = Menu.tabAnchor:GetWidth()
 
@@ -299,54 +234,18 @@ function unitFramesTab:Create()
         self:SetUnitPage(unit)
     end)
 
-    -- Widget Buttons
-    self.widgetPane = Cell:CreateTitledPane(self.unitSection, L.Widgets,
-        sectionWidth, self.paneHeight)
-    self.widgetPane:SetPoint("TOPLEFT", self.unitSection, "BOTTOMLEFT")
+    -- Tabs
+    self.tabPane = Cell:CreateTitledPane(self.window, nil, sectionWidth, self.paneHeight)
+    self.tabPane:SetPoint("TOPLEFT", self.unitSection, "BOTTOMLEFT")
 
     -- Repoint so it's anchored to bottom
-    self.widgetPane.line:ClearAllPoints()
-    self.widgetPane.line:SetPoint("BOTTOMLEFT", self.widgetPane, "BOTTOMLEFT")
-    self.widgetPane.line:SetPoint("BOTTOMRIGHT", self.widgetPane, "BOTTOMRIGHT")
+    self.tabPane.line:ClearAllPoints()
+    self.tabPane.line:SetPoint("BOTTOMLEFT", self.tabPane, "BOTTOMLEFT")
+    self.tabPane.line:SetPoint("BOTTOMRIGHT", self.tabPane, "BOTTOMRIGHT")
 
-    -- settings frame
-    ---@class UnitsFramesTab.settingsFrame: Frame
-    ---@field scrollFrame CellScrollFrame
-    self.settingsFrame = CUF:CreateFrame("CUF_Menu_UnitFrame_Widget", self.widgetPane,
-        sectionWidth,
-        (self.widgetHeight - self.paneHeight), true, true)
-    self.settingsFrame:SetPoint("TOPLEFT", self.widgetPane, "BOTTOMLEFT", 0, -inset)
+    --local anchorWidth = self.baseWidth - gap
+    self.tabAnchor = CUF:CreateFrame("TabAnchor", self.tabPane, sectionWidth, 1, true)
+    self.tabAnchor:SetPoint("TOPLEFT", self.tabPane, "BOTTOMLEFT", 0, -10)
 
-    Cell:CreateScrollFrame(self.settingsFrame)
-    self.settingsFrame.scrollFrame:SetScrollStep(50)
-
-    self:InitWidgets()
-
-    ---@class UnitsFramesTab.widgetListFrame: CellCombatFrame
-    local widgetListWindow = CUF:CreateFrame("CUF_Menu_UnitFrame_WidgetList", self.window,
-        sectionWidth / 3,
-        self.settingsFrame:GetHeight(), false, true)
-    widgetListWindow:SetPoint("BOTTOMRIGHT", self.window, "BOTTOMLEFT", 1 - inset, 0)
-
-    -- Give the widget list a mask and hook it to the menu's mask
-    F:ApplyCombatProtectionToFrame(widgetListWindow)
-    Cell:CreateMask(widgetListWindow, nil, { 1, -1, -1, 1 })
-    widgetListWindow.mask:Hide()
-
-    hooksecurefunc(Menu.window.mask, "Show", function()
-        widgetListWindow.mask:Show()
-    end)
-    hooksecurefunc(Menu.window.mask, "Hide", function()
-        widgetListWindow.mask:Hide()
-    end)
-
-    ---@class UnitsFramesTab.widgetListFrame: Frame
-    ---@field scrollFrame CellScrollFrame
-    self.widgetListFrame = CUF:CreateFrame("CUF_Menu_UnitFrame_WidgetListFrame", widgetListWindow,
-        widgetListWindow:GetWidth(),
-        widgetListWindow:GetHeight(), false, true)
-    self.widgetListFrame:SetPoint("TOPLEFT", widgetListWindow, "TOPLEFT", 0, 0)
-
-    Cell:CreateScrollFrame(self.widgetListFrame)
-    self.widgetListFrame.scrollFrame:SetScrollStep(25)
+    self:InitTabs()
 end
---
+++ Menu/WidgetsTab.lua
@@ -0,0 +1,250 @@
+---@class CUF
+local CUF = select(2, ...)
+
+local Cell = CUF.Cell
+local F = Cell.funcs
+local L = CUF.L
+
+local Builder = CUF.Builder
+
+---@class CUF.Menu
+local Menu = CUF.Menu
+
+local unitFramesTab = Menu.unitFramesTab
+
+---@class WidgetsTab: Menu.Tab
+---@field widgetsToAdd table<number, WidgetMenuPage.Args>
+---@field listButtons table<WIDGET_KIND, CellButton>
+---@field widgetPages table<WIDGET_KIND, WidgetMenuPage>
+local WidgetsTab = {}
+WidgetsTab.widgetPages = {}
+WidgetsTab.listButtons = {}
+WidgetsTab.widgetsToAdd = {}
+WidgetsTab.id = "Widgets"
+WidgetsTab.height = 390
+WidgetsTab.paneHeight = 17
+
+unitFramesTab:AddTab(WidgetsTab)
+
+-------------------------------------------------
+-- MARK: Show/Hide
+-------------------------------------------------
+
+---@param unit Unit
+function WidgetsTab:ShowTab(unit)
+    --CUF:Log("|cff00ccffShow generalTab|r")
+    if not self.window then
+        self:Create()
+
+        self.window:Show()
+        self.LoadWidgetList(unit)
+
+        self.listButtons[self.firstWidgetInList]:Click()
+
+        self.init = true
+        return
+    end
+
+    self.window:Show()
+end
+
+function WidgetsTab:HideTab()
+    if not self.window or not self.window:IsShown() then return end
+    --CUF:Log("|cff00ccffHide generalTab|r")
+    self.window:Hide()
+end
+
+function WidgetsTab:IsShown()
+    return WidgetsTab.window and WidgetsTab.window:IsShown()
+end
+
+-- Update the selected widge
+---@param widget WIDGET_KIND
+function WidgetsTab:SetWidget(widget)
+    -- Hide old widget
+    if self.selectedWidget then
+        self.selectedWidget.frame:Hide()
+    end
+
+    self.selectedWidget = self.widgetPages[widget]
+    self.selectedWidget.frame:Show()
+
+    self.selectedWidget.frame:ClearAllPoints()
+    self.selectedWidget.frame:SetPoint("TOPLEFT", self.settingsFrame.scrollFrame.content)
+
+    -- Extremely dirty hack, but without this options will sometimes not be shown
+    C_Timer.After(0.1, function()
+        self.settingsFrame.scrollFrame:SetContentHeight(self.selectedWidget.height)
+        self.settingsFrame.scrollFrame:ResetScroll()
+    end)
+
+    CUF.Menu:UpdateSelectedPages(nil, widget)
+end
+
+-------------------------------------------------
+-- MARK: Init
+-------------------------------------------------
+
+function WidgetsTab:InitWidgets()
+    for _, widget in pairs(self.widgetsToAdd) do
+        local widgetPage = Builder:CreateWidgetMenuPage(self.settingsFrame, widget.widgetName, unpack(widget.options))
+
+        self.widgetPages[widgetPage.id] = widgetPage
+    end
+end
+
+---@param widgetName WIDGET_KIND
+---@param ... MenuOptions
+function Menu:AddWidget(widgetName, ...)
+    table.insert(WidgetsTab.widgetsToAdd,
+        { ["widgetName"] = widgetName, ["options"] = { ... } })
+end
+
+-------------------------------------------------
+-- MARK: Callbacks
+-------------------------------------------------
+
+---@param unit Unit
+function WidgetsTab.LoadWidgetList(unit)
+    if not WidgetsTab:IsShown() then return end
+
+    WidgetsTab.widgetListFrame.scrollFrame:Reset()
+
+    local optionCount = 0
+    local widgetTable = CUF.DB.GetSelectedWidgetTables(unit)
+    local prevButton
+    WidgetsTab.firstWidgetInList = nil
+
+    -- The list is ordered by load order from .toc
+    for _, widgetPage in pairs(WidgetsTab.widgetsToAdd) do
+        local widgetName = widgetPage.widgetName
+        local widget = widgetTable[widgetName]
+
+        if widget then
+            if not WidgetsTab.listButtons[widgetName] then
+                WidgetsTab.listButtons[widgetName] = CUF:CreateButton(
+                    WidgetsTab.widgetListFrame.scrollFrame.content, " ",
+                    { 20, 20 }, nil, "transparent-accent")
+            end
+
+            ---@class WidgetMenuPageButton: CellButton
+            local button = WidgetsTab.listButtons[widgetName]
+            button:SetText(L[widgetName])
+            button:GetFontString():ClearAllPoints()
+            button:GetFontString():SetPoint("LEFT", 5, 0)
+            button:GetFontString():SetPoint("RIGHT", -5, 0)
+
+            button.id = widgetName
+            optionCount = optionCount + 1
+
+            if widget.enabled then
+                button:SetTextColor(1, 1, 1, 1)
+            else
+                button:SetTextColor(0.466, 0.466, 0.466, 1)
+            end
+
+            button:SetParent(WidgetsTab.widgetListFrame.scrollFrame.content)
+            button:SetPoint("RIGHT")
+            if not prevButton then
+                button:SetPoint("TOPLEFT")
+            else
+                button:SetPoint("TOPLEFT", prevButton, "BOTTOMLEFT", 0, 1)
+            end
+            button:Show()
+
+            prevButton = button
+            if not WidgetsTab.firstWidgetInList then
+                WidgetsTab.firstWidgetInList = widgetName
+            end
+        end
+    end
+
+    WidgetsTab.widgetListFrame.scrollFrame:SetContentHeight(20, optionCount, -1)
+
+    Cell:CreateButtonGroup(WidgetsTab.listButtons, function(widget, b)
+        WidgetsTab:SetWidget(widget)
+    end)
+
+    -- Make sure that the currently selected widget is valid
+    if WidgetsTab.selectedWidget then
+        if not widgetTable[WidgetsTab.selectedWidget.id] then
+            WidgetsTab.listButtons[WidgetsTab.firstWidgetInList]:Click()
+        end
+    end
+end
+
+CUF:RegisterCallback("LoadPageDB", "unitFramesTab_LoadWidgetList", WidgetsTab.LoadWidgetList)
+
+---@param layout string?
+---@param unit Unit?
+---@param widgetName WIDGET_KIND?
+function WidgetsTab.UpdateWidgetListEnabled(layout, unit, widgetName, setting)
+    if not WidgetsTab:IsShown() then return end
+    if not widgetName then return end
+    if not setting == CUF.constants.OPTION_KIND.ENABLED then return end
+    if not WidgetsTab.listButtons[widgetName] then return end
+
+    if CUF.DB.GetSelectedWidgetTable(widgetName, unit).enabled then
+        WidgetsTab.listButtons[widgetName]:SetTextColor(1, 1, 1, 1)
+    else
+        WidgetsTab.listButtons[widgetName]:SetTextColor(0.466, 0.466, 0.466, 1)
+    end
+end
+
+CUF:RegisterCallback("UpdateWidget", "WidgetsTab_UpdateWidgetListEnabled", WidgetsTab.UpdateWidgetListEnabled)
+
+-------------------------------------------------
+-- MARK: Create
+-------------------------------------------------
+
+-- Create
+function WidgetsTab:Create()
+    --CUF:Log("|cff00ccffCreate generalTab|r")
+
+    local sectionWidth = unitFramesTab.tabAnchor:GetWidth()
+
+    self.window = CUF:CreateFrame("CUF_Menu_UnitFrame_Widget", unitFramesTab.window,
+        sectionWidth,
+        self.height, true)
+    self.window:SetPoint("TOPLEFT", unitFramesTab.tabAnchor, "TOPLEFT")
+
+    -- settings frame
+    ---@class UnitsFramesTab.settingsFrame: Frame
+    ---@field scrollFrame CellScrollFrame
+    self.settingsFrame = CUF:CreateFrame("CUF_Menu_UnitFrame_Widget_Settings", self.window,
+        sectionWidth, self.height, true, true)
+    self.settingsFrame:SetPoint("TOPLEFT")
+
+    Cell:CreateScrollFrame(self.settingsFrame)
+    self.settingsFrame.scrollFrame:SetScrollStep(50)
+
+    self:InitWidgets()
+
+    ---@class UnitsFramesTab.widgetListFrame: CellCombatFrame
+    local widgetListWindow = CUF:CreateFrame("CUF_Menu_UnitFrame_WidgetList", self.window,
+        sectionWidth / 3,
+        self.settingsFrame:GetHeight(), false, true)
+    widgetListWindow:SetPoint("BOTTOMRIGHT", self.window, "BOTTOMLEFT", -5 + 1, 0)
+
+    -- Give the widget list a mask and hook it to the menu's mask
+    F:ApplyCombatProtectionToFrame(widgetListWindow)
+    Cell:CreateMask(widgetListWindow, nil, { 1, -1, -1, 1 })
+    widgetListWindow.mask:Hide()
+
+    hooksecurefunc(Menu.window.mask, "Show", function()
+        widgetListWindow.mask:Show()
+    end)
+    hooksecurefunc(Menu.window.mask, "Hide", function()
+        widgetListWindow.mask:Hide()
+    end)
+
+    ---@class UnitsFramesTab.widgetListFrame: Frame
+    ---@field scrollFrame CellScrollFrame
+    self.widgetListFrame = CUF:CreateFrame("CUF_Menu_UnitFrame_WidgetListFrame", widgetListWindow,
+        widgetListWindow:GetWidth(),
+        widgetListWindow:GetHeight(), false, true)
+    self.widgetListFrame:SetPoint("TOPLEFT", widgetListWindow, "TOPLEFT", 0, 0)
+
+    Cell:CreateScrollFrame(self.widgetListFrame)
+    self.widgetListFrame.scrollFrame:SetScrollStep(25)
+end

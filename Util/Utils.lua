efcdab520a54d9beae3940c3b708bf7d33b60c38
Vollmer <kristianvollmer@gmail.com>
2024-10-29 11:41:34 +0100

    [Bugfix] Fix "Invalid UTF-8 character" error message when determining string lengths

--- Util/Utils.lua
+++ Util/Utils.lua
@@ -162,6 +162,153 @@ function Util:IterateAllUnitButtons(func, unitToIterate, ...)
 end
 
 -------------------------------------------------
+-- MARK: Compat
+-------------------------------------------------
+
+--[[
+Copyright (c) 2006-2007, Kyle Smith
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of its contributors may be
+      used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--]]
+
+-- Below are compatted versions of some of the uft8 fns from Cell that will fail more gracefully
+
+-- ABNF from RFC 3629
+--
+-- UTF8-octets = *( UTF8-char )
+-- UTF8-char   = UTF8-1 / UTF8-2 / UTF8-3 / UTF8-4
+-- UTF8-1      = %x00-7F
+-- UTF8-2      = %xC2-DF UTF8-tail
+-- UTF8-3      = %xE0 %xA0-BF UTF8-tail / %xE1-EC 2( UTF8-tail ) /
+--               %xED %x80-9F UTF8-tail / %xEE-EF 2( UTF8-tail )
+-- UTF8-4      = %xF0 %x90-BF 2( UTF8-tail ) / %xF1-F3 3( UTF8-tail ) /
+--               %xF4 %x80-8F 2( UTF8-tail )
+-- UTF8-tail   = %x80-BF
+
+-- returns the number of bytes used by the UTF-8 character at byte i in s
+-- also doubles as a UTF-8 character validator (this part is compatted to return 1 instead of erroring)
+---@param s string
+---@param i number
+---@return number
+local function utf8charbytes(s, i)
+    -- argument defaults
+    i = i or 1
+
+    -- argument checking
+    if type(s) ~= "string" then
+        error("bad argument #1 to 'utf8charbytes' (string expected, got " .. type(s) .. ")")
+    end
+    if type(i) ~= "number" then
+        error("bad argument #2 to 'utf8charbytes' (number expected, got " .. type(i) .. ")")
+    end
+
+    local c = s:byte(i)
+
+    -- determine bytes needed for character, based on RFC 3629
+    -- validate byte 1
+    if c > 0 and c <= 127 then
+        -- UTF8-1
+        return 1
+    elseif c >= 194 and c <= 223 then
+        -- UTF8-2
+        local c2 = s:byte(i + 1)
+        if not c2 or c2 < 128 or c2 > 191 then
+            -- Invalid UTF-8 sequence, skip 1 byte
+            return 1
+        end
+        return 2
+    elseif c >= 224 and c <= 239 then
+        -- UTF8-3
+        local c2 = s:byte(i + 1)
+        local c3 = s:byte(i + 2)
+
+        if not c2 or not c3 then
+            -- UTF-8 string terminated early, skip 1 byte
+            return 1
+        end
+
+        -- validate byte 2 and byte 3 based on c
+        if (c == 224 and (c2 < 160 or c2 > 191)) or
+            (c == 237 and (c2 < 128 or c2 > 159)) or
+            (c2 < 128 or c2 > 191) or
+            (c3 < 128 or c3 > 191) then
+            -- Invalid UTF-8 sequence, skip 1 byte
+            return 1
+        end
+
+        return 3
+    elseif c >= 240 and c <= 244 then
+        -- UTF8-4
+        local c2 = s:byte(i + 1)
+        local c3 = s:byte(i + 2)
+        local c4 = s:byte(i + 3)
+
+        if not c2 or not c3 or not c4 then
+            -- UTF-8 string terminated early, skip 1 byte
+            return 1
+        end
+
+        -- validate byte 2, byte 3, and byte 4
+        if (c == 240 and (c2 < 144 or c2 > 191)) or
+            (c == 244 and (c2 < 128 or c2 > 143)) or
+            (c2 < 128 or c2 > 191) or
+            (c3 < 128 or c3 > 191) or
+            (c4 < 128 or c4 > 191) then
+            -- Invalid UTF-8 sequence, skip 1 byte
+            return 1
+        end
+
+        return 4
+    else
+        -- Invalid first byte, skip 1 byte
+        return 1
+    end
+end
+
+-- returns the number of characters in a UTF-8 string
+---@param s string
+---@return number
+local function utf8len(s)
+    -- argument checking
+    if type(s) ~= "string" then
+        error("bad argument #1 to 'utf8len' (string expected, got " .. type(s) .. ")")
+    end
+
+    local pos = 1
+    local bytes = s:len()
+    local len = 0
+
+    while pos <= bytes do
+        len = len + 1
+        pos = pos + utf8charbytes(s, pos)
+    end
+
+    return len
+end
+
+-------------------------------------------------
 -- MARK: Util
 -------------------------------------------------
 
@@ -177,16 +324,16 @@ function Util.UpdateTextWidth(fs, text, widthTable, relativeTo)
     elseif widthTable.type == "percentage" then
         local percent = widthTable.value or 0.75
         local width = relativeTo:GetWidth() - 2
-        for i = string.utf8len(text), 0, -1 do
+        for i = utf8len(text), 0, -1 do
             fs:SetText(string.utf8sub(text, 1, i))
             if fs:GetWidth() / width <= percent then
                 break
             end
         end
     elseif widthTable.type == "length" then
-        if string.len(text) == string.utf8len(text) then -- en
+        if string.len(text) == utf8len(text) then -- en
             fs:SetText(string.utf8sub(text, 1, widthTable.value))
-        else                                             -- non-en
+        else                                      -- non-en
             fs:SetText(string.utf8sub(text, 1, widthTable.auxValue))
         end
     end

7fca14fb97f202f2af5e34ad5d12dcb378fed79a
Vollmer <kristianvollmer@gmail.com>
2024-11-08 13:50:10 +0100

    remove useScaling check for "scale" callback

--- UnitFrames/UnitButton.lua
+++ UnitFrames/UnitButton.lua
@@ -288,7 +288,7 @@ local function UpdateAppearance(kind)
             end)
         end)
     end
-    if kind == "scale" and CUF_DB.useScaling then
+    if kind == "scale" then
         -- Full update for everything
         -- Needs to be delayed
         C_Timer.After(0.1, function()

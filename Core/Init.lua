b71f8569b82da290e8ffc07043a991570fd10005
Vollmer <kristianvollmer@gmail.com>
2025-01-18 14:44:36 +0100

    add version vars

--- Core/Init.lua
+++ Core/Init.lua
@@ -43,6 +43,16 @@ CUF.Compat = {}
 ---@field useScaling boolean
 CUF.vars = {}
 
+-- Borrowed from DBM https://github.com/DeadlyBossMods/DeadlyBossMods/blob/716071c1e561174a1f60b3cd36a000bac267027b/DBM-Core/modules/objects/GameVersion.lua#
+CUF.vars.isRetail = WOW_PROJECT_ID == (WOW_PROJECT_MAINLINE or 1)
+CUF.vars.isClassic = WOW_PROJECT_ID == (WOW_PROJECT_CLASSIC or 2)
+CUF.vars.isHardcoreServer = C_GameRules and C_GameRules.IsHardcoreActive and C_GameRules.IsHardcoreActive()
+--[[ CUF.vars.currentSeason = WOW_PROJECT_ID == (WOW_PROJECT_CLASSIC or 2) and C_Seasons and C_Seasons.HasActiveSeason() and
+C_Seasons.GetActiveSeason() ]]
+CUF.vars.isBCC = WOW_PROJECT_ID == (WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5)
+CUF.vars.isWrath = WOW_PROJECT_ID == (WOW_PROJECT_WRATH_CLASSIC or 11)
+CUF.vars.isCata = WOW_PROJECT_ID == (WOW_PROJECT_CATACLYSM_CLASSIC or 14)
+
 ---@class CUF.unitButtons
 ---@field [Unit] CUFUnitButton
 ---@field boss table<string, CUFUnitButton>

ef36d51bf53276b2d50e2e50bd0e7ecc1f87056a
Vollmer <kristianvollmer@gmail.com>
2025-03-01 20:31:16 +0100

    Update unit frame options

--- Menu/UnitFramesTab.lua
+++ Menu/UnitFramesTab.lua
@@ -21,7 +21,7 @@ unitFramesTab.id = "unitFramesTab"
 unitFramesTab.unitPages = {}
 unitFramesTab.unitsToAdd = {}
 unitFramesTab.unitPageButtons = {}
-unitFramesTab.unitHeight = 200
+unitFramesTab.unitHeight = 160
 unitFramesTab.paneHeight = 17
 
 Menu.unitFramesTab = unitFramesTab
